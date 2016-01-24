9ec58a2ed39481a6aed7ae6a2fe8c5b2c394fb39
Joe Bordes <joe@tsolucio.com>
2016-01-24 20:15:25 +0100

    i18n(StructedData) translate extension fields

---
+++ conf/lang/en/data-plugin.php
@@ -0,0 +1,24 @@
+<?php
+$lang['name']  = 'Name';
+$lang['type'] = 'Type';
+$lang['description'] = 'Description';
+$lang['keywords'] = 'Keywords';
+$lang['version'] = 'Version';
+$lang['homepage'] = 'Home Page';
+$lang['release'] = 'Release Date';
+$lang['license'] = 'License';
+$lang['price'] = 'Price';
+$lang['buyemail'] = 'Purchase EMail';
+$lang['buyurl'] = 'Purchase URL';
+$lang['distribution'] = 'Distribution';
+$lang['authorname'] = 'Author Name';
+$lang['authoremmail'] = 'Author EMail';
+$lang['authorhomepage'] = 'Author Home Page';
+$lang['supportemail'] = 'Support EMail';
+$lang['supportissues'] = 'Support Issues';
+$lang['supportforum'] = 'Support Forum';
+$lang['supportwiki'] = 'Support Wiki';
+$lang['supportirc'] = 'Support IRC';
+$lang['supportsource'] = 'Source';
+$lang['supportdocs'] = 'Documentation';
+?>
---
+++ conf/lang/es/data-plugin.php
@@ -0,0 +1,24 @@
+<?php
+$lang['name']  = 'Nombre';
+$lang['type'] = 'Type';
+$lang['description'] = 'Descripción';
+$lang['keywords'] = 'Palabras Claves';
+$lang['version'] = 'Versión';
+$lang['homepage'] = 'Página Web';
+$lang['release'] = 'Fecha Publicación';
+$lang['license'] = 'Licencia';
+$lang['price'] = 'Precio';
+$lang['buyemail'] = 'EMail Compra';
+$lang['buyurl'] = 'URL Compra';
+$lang['distribution'] = 'Distribución';
+$lang['authorname'] = 'Nombre Autor';
+$lang['authoremmail'] = 'EMail Autor';
+$lang['authorhomepage'] = 'Página Web Autor';
+$lang['supportemail'] = 'EMail de Soporte';
+$lang['supportissues'] = 'Incidencias de Soporte';
+$lang['supportforum'] = 'Foro de Soporte';
+$lang['supportwiki'] = 'Wiki de Soporte';
+$lang['supportirc'] = 'IRC de Soporte';
+$lang['supportsource'] = 'Código Fuente';
+$lang['supportdocs'] = 'Documentación';
+?>
